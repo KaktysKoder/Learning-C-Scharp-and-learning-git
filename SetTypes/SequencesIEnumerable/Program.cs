0aa2f4df6034b73a95d1fb33d1d150e9d3665454
Rimuru <kaktysKoder@gmail.com>
2020-06-28 16:51:19 +0400

    Перенос проекта в другую папку -> родительскую.

---
+++ SetTypes/SequencesIEnumerable/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SetTypes.ICollection;
+
+namespace SetTypes
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            ICollectionTest();
+
+            Console.ReadLine();
+        }
+
+        private static void SequencesIEnumerableTest()
+        {
+            Progression progression = new Progression(100);
+
+            foreach (int i in progression)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+        private static void ICollectionTest()
+        {
+            StoreCollection collection = new StoreCollection(@"F:\test.txt");
+
+            collection.Add(2020);
+            collection.Remove(188);
+
+            foreach(int i in collection)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
